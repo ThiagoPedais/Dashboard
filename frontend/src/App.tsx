29bec85bcd4042190314b2ba283881804132c96f
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-08-06 15:55:51 -0300

    Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
 
 function App() {
 
     return (
-        <div>
-           <h1>Hello World!!</h1>
-        </div>
+        <>
+           <Header />
+        </>
     )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+export default function Header() {
+  return (
+    <header className="main-header-container">
+        <h1 className="main-header-title">DS Sales</h1>
+    </header>
+  );
+}
